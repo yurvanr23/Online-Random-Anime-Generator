30ddd85f94cbb93428ee361f955a8fd367af26d3
Yurvan <yurvanr23@gmail.com>
2026-01-12 11:49:12 +0200

    Added dark mode, skeleton loader, and genre pills UI
    Introduces a dark mode toggle, skeleton loading animation, and genre pills for improved user experience. Refactored CSS to use CSS variables for themes, updated HTML structure for new features, and enhanced JS to handle dark mode, loading state, and genre pill rendering.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,18 +1,45 @@
 import { fetchRandomAnime } from "./api.js";
 import { displayAnime } from "./ui.js";
 
+const loader = document.getElementById("loader");
+const card = document.querySelector(".card");
+const button = document.getElementById("generate");
+
 document.getElementById("generate").addEventListener("click", async () => {
-    
     try {
+        button.disabled = true;
+
+        // scrolls to top
+        window.scrollTo({ 
+            top: 0, 
+            behavior: 'smooth' 
+        });
+
+        loader.classList.remove("hidden");
+
         const response = await fetch("http://127.0.0.1:8000/api/anime/random");
         //const anime = await response.json();
         const anime = await fetchRandomAnime();
+
+        card.classList.remove("fade");
+        void card.offsetWidth; // restart animation
+        card.classList.add("fade");
+        
         console.log(anime); // debug to see if it works
         displayAnime(anime);
+
     } catch (err) {
         console.error(err);
+    } finally {
+        loader.classList.add("hidden");
+        button.disabled = false;
     }
     //displayAnime(anime);
+});
+
+ // Dark Mode
+document.getElementById("themeToggle").addEventListener("click", () => {
+    document.body.classList.toggle("dark");
 });
 
 /*document.getElementById("generate").addEventListener("click", async () => {
--- frontend/js/ui.js
+++ frontend/js/ui.js
@@ -3,6 +3,17 @@ export function displayAnime(anime) {
     document.getElementById("synopsis").textContent = anime.synopsis || "No synopsis available.";
     document.getElementById("score").textContent = anime.score ? `Score: ${anime.score}` : "Score: N/A";
     document.getElementById("episodes").textContent = anime.episodes ? `Episodes: ${anime.episodes}` : "Episodes: N/A";
-    document.getElementById("genres").textContent = anime.genres.length ? `Genres: ${anime.genres.join(", ")}` : "Genres: N/A";
+    //document.getElementById("genres").textContent = anime.genres.length ? `Genres: ${anime.genres.join(", ")}` : "Genres: N/A";
     document.getElementById("image_url").src = anime.image_url || "placeholder.jpg";
+
+    // Genre Pills
+    const genreContainer = document.getElementById("genres");
+    genreContainer.innerHTML = ""; // Clear previous genres
+
+    anime.genres.forEach(genre => {
+        const pill = document.createElement("span");
+        pill.className = "genre-pill";
+        pill.textContent = genre;
+        genreContainer.appendChild(pill);
+    });
 }
