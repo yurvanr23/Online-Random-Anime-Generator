6fef45c77f44d93c5ac2d37371d3863b15b85d21
Yurvan <146588193+yurvanr23@users.noreply.github.com>
2026-02-02 08:33:08 +0200

    Update API_URL to use production endpoint

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,6 @@
-const API_URL = "http://127.0.0.1:8000/api/anime/random";
-
+//const API_URL = "http://127.0.0.1:8000/api/anime/random";
+const API_URL = "https://random-anime-generator.onrender.com/api/anime/random";
+    
 export async function fetchRandomAnime() { 
     const response = await fetch(API_URL);
     return response.json();
